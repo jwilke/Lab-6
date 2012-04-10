35b08ad9ab0332a5066a1dc02088f9c288a0e0ff
Graham Benevelli <grahambenevelli@gmail.com>
2012-04-09 20:11:14 -0500

    DiskU

--- labFS/DiskUnit.java
+++ labFS/DiskUnit.java
@@ -37,7 +37,7 @@ public class DiskUnit implements DiskCallback{
 	//-------------------------------------------------------
 	// main() -- java DiskUnit to run this test
 	//-------------------------------------------------------
-	public static void main(String args[])
+	public static void main(String args[]) throws IllegalArgumentException, IOException
 	{
 		/*testWrites(true, (float)0.0, -1);
     System.out.println("Wrote 0 to all");
@@ -58,7 +58,31 @@ public class DiskUnit implements DiskCallback{
     testReads(false, -1);    
     System.out.println("Read data from some");
     System.exit(0);*/
+		Tester t = new Tester();
+		System.out.println("Testing TranID");
+		TransID id = new TransID();
+		id.unit(t);
 		
+		System.out.println("Testing Write");
+		Write w = new Write(0, new byte[0]);
+		w.unit(t);
+		
+		System.out.println("Testing Transaction");
+		Transaction tran = new Transaction();
+		tran.unit(t);
+		
+		System.out.println("Testing ActiveTransactionList");
+		ActiveTransactionList atl = new ActiveTransactionList();
+		atl.unit(t);
+		
+		System.out.println("Testing WrtieBackList");
+		WriteBackList.unit(t);
+		
+		//System.out.println("Testing LogStatus");
+		//LogStatus.unit(t);
+		
+		System.out.println();
+		t.close();
 	}
 
 
